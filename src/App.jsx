080dc9e9cc26ee40867d0c43f7c02b915dab4ca5
elangbayu <elangbayu17@gmail.com>
2023-08-18 16:34:12 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Testimonials />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
