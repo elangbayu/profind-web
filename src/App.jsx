0ebbe896f5c407493739de0137cbbfacdf5f6585
elangbayu <elangbayu17@gmail.com>
2023-08-20 15:00:35 +0700

    feat: add internationalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,75 @@
+import { useEffect } from "react";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
+import { useTranslation } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+function getBrowserLocales(options = {}) {
+  const defaultOptions = {
+    languageCodeOnly: true,
+  };
+
+  const opt = {
+    ...defaultOptions,
+
+    ...options,
+  };
+
+  const browserLocales =
+    navigator.languages === undefined
+      ? [navigator.language]
+      : navigator.languages;
+
+  if (!browserLocales) {
+    return undefined;
+  }
+
+  return browserLocales.map((locale) => {
+    const trimmedLocale = locale.trim();
+
+    return opt.languageCodeOnly ? trimmedLocale.split(/-|_/)[0] : trimmedLocale;
+  });
+}
+
+const i18nextOptions = {
+  order: [
+    "localStorage",
+    "querystring",
+    "cookie",
+    "sessionStorage",
+    "navigator",
+    "htmlTag",
+  ],
+  lookupQuerystring: "lng",
+  lookupCookie: "i18next",
+  lookupLocalStorage: "i18nextLng",
+  lookupSessionStorage: "i18nextLng",
+
+  // cache user language
+  // caches: ["localStorage"],
+  excludeCacheFor: ["cimode"],
+  //cookieMinutes: 10,
+  //cookieDomain: 'myDomain'
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    localStorage.setItem("i18nextLng", getBrowserLocales()[0]);
+    i18n.use(LanguageDetector).init({
+      detection: i18nextOptions,
+    });
+  }, []);
+
   return (
     <>
-      <Hero />
-      <Features />
-      <Testimonials />
-      <Contact />
+      <Hero translate={t} />
+      <Features translate={t} />
+      <Testimonials translate={t} />
+      <Contact translate={t} />
     </>
   );
 }
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,44 +1,59 @@
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+/* eslint-disable react/prop-types */
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+} from "@heroicons/react/24/outline";
 
-const features = [
-  {
-    name: 'Push to deploy',
-    description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates',
-    description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues',
-    description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security',
-    description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
-  },
-]
+export default function Features({ translate }) {
+  const features = [
+    {
+      name: "Extensive Property Listings",
+      description: translate("Feature - Extensive Property Listings"),
+      icon: CloudArrowUpIcon,
+    },
+    {
+      name: "Personalized Recommendations",
+      description: translate("Feature - Personalized Recommendations"),
+      icon: LockClosedIcon,
+    },
+    {
+      name: "Virtual Tours",
+      description: translate("Feature - Virtual Tours"),
+      icon: ArrowPathIcon,
+    },
+    {
+      name: "Expert Real Estate Agents",
+      description: translate("Feature - Expert Real Estate Agents"),
+      icon: FingerPrintIcon,
+    },
+    {
+      name: "Neighborhood Insights",
+      description: translate("Feature - Neighborhood Insights"),
+      icon: FingerPrintIcon,
+    },
+    {
+      name: "Save Favorites and Alerts",
+      description: translate("Feature - Save Favorites and Alerts"),
+      icon: FingerPrintIcon,
+    },
+    {
+      name: "Seamless Mobile Experience",
+      description: translate("Feature - Seamless Mobile Experience"),
+      icon: FingerPrintIcon,
+    },
+  ];
 
-export default function Example() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Your #1 source of finding cheap and good properties.
+            {translate("Features Heading")}
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+            {translate("Features Subheading")}
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
@@ -47,16 +62,21 @@ export default function Example() {
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                    <feature.icon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {feature.description}
+                </dd>
               </div>
             ))}
           </dl>
         </div>
       </div>
     </div>
-  )
+  );
 }
