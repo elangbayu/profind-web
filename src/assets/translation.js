7dcb4fa982f66266b31b831d64df757359b89f26
elangbayu <elangbayu17@gmail.com>
2023-08-20 15:00:35 +0700

    feat: add internationalization

---
+++ src/assets/translation.js
@@ -0,0 +1,70 @@
+export const resources = {
+  en: {
+    translation: {
+      Tagline: "Discover Your Dream Home with Profind.",
+      "Hero Short Description":
+        "Welcome to the ultimate destination for finding your perfect property! Whether you're looking for a cozy apartment, a spacious family home, or an elegant villa, Profind is here to turn your property dreams into reality.",
+      "Navbar Features": "Features",
+      "Navbar Company": "Company",
+      "Login Button": "Log in",
+      "Breadcrumbs Main": "More locations just open!",
+      "Breadcrumbs Action": "Find out",
+      "Hero CTA": "Search for property",
+      "Features Heading":
+        "Your #1 source of finding cheap and good properties.",
+      "Features Subheading":
+        "Explore a vast selection of listings, connect with trusted real estate agents, and embark on a journey to find the home that truly suits your lifestyle.",
+      "Feature - Extensive Property Listings":
+        "Explore a diverse range of properties at your fingertips. From urban condos to serene countryside retreats, [Website Name] boasts an extensive collection of listings to cater to your unique preferences. Our intuitive search filters help you narrow down your choices and discover properties that match your criteria effortlessly.",
+      "Feature - Personalized Recommendations":
+        "Our advanced algorithm understands your preferences and provides personalized property recommendations that align with your tastes and requirements. Say goodbye to endless scrolling and hello to targeted results that make your property hunt enjoyable and efficient.",
+      "Feature - Virtual Tours":
+        "Experience properties from the comfort of your current space. Our virtual tours offer an immersive walkthrough of homes, allowing you to visualize yourself living in each space before scheduling an in-person visit. Saving you time and ensuring you focus on properties that truly resonate with you.",
+      "Feature - Expert Real Estate Agents":
+        "Connect with experienced real estate professionals who are dedicated to helping you make informed decisions. Our network of agents is ready to guide you through every step of the buying process, providing insights, answering questions, and negotiating on your behalf.",
+      "Feature - Neighborhood Insights":
+        "Make an informed choice not only about the property but also the neighborhood it's in. [Website Name] provides valuable insights into local communities, schools, amenities, and more, ensuring you're not just buying a house, but a lifestyle.",
+      "Feature - Save Favorites and Alerts":
+        "Easily keep track of properties that catch your eye by saving them to your favorites. Plus, set up alerts to stay updated on new listings and price changes, so you never miss an opportunity to find your dream home.",
+      "Feature - Seamless Mobile Experience":
+        "Search for properties on the go with our mobile-responsive website. Whether you're commuting or relaxing at home, [Website Name] ensures that your property search remains smooth and accessible from any device.",
+      "Trusted by": "Trusted by the world's most richest",
+      "Contact description":
+        "Start your property journey with Profind today and uncover a world of possibilities. Your dream home is just a click away!",
+    },
+  },
+  id: {
+    translation: {
+      Tagline: "Temukan Rumah Impian Anda bersama Profind.",
+      "Hero Short Description":
+        "Selamat datang di destinasi utama untuk menemukan properti yang sempurna! Anda sedang mencari apartemen yang nyaman, rumah keluarga yang luas, atau vila yang elegan? Jangan khawatir, Profind hadir untuk mengubah impian properti Anda menjadi kenyataan.",
+      "Navbar Features": "Keunggulan",
+      "Navbar Company": "Perusahaan",
+      "Login Button": "Masuk",
+      "Breadcrumbs Main": "Banyak lokasi baru dibuka!",
+      "Breadcrumbs Action": "Temukan",
+      "Hero CTA": "Cari properti",
+      "Features Heading":
+        "Sumber terpercaya untuk menemukan rumah impian Anda.",
+      "Features Subheading":
+        "Jelajahi beragam daftar properti, terhubunglah dengan agen real estat terpercaya, dan mulailah perjalanan untuk menemukan rumah yang benar-benar cocok dengan gaya hidup Anda.",
+      "Feature - Extensive Property Listings":
+        "Jelajahi berbagai macam properti dengan mudah. Dari kondominium perkotaan hingga tempat peristirahatan pedesaan yang tenang, Profind memiliki koleksi daftar properti yang luas untuk memenuhi preferensi unik Anda. Filter pencarian intuitif kami membantu Anda menyaring pilihan Anda dan menemukan properti yang sesuai dengan kriteria Anda tanpa usaha berlebihan.",
+      "Feature - Personalized Recommendations":
+        "Algoritma canggih kami memahami preferensi Anda dan memberikan rekomendasi properti yang dipersonalisasi sesuai dengan selera dan kebutuhan Anda. Selamat tinggal scrolling tak berujung dan selamat datang hasil yang ditargetkan yang membuat pencarian properti Anda menyenangkan dan efisien.",
+      "Feature - Virtual Tours":
+        "Pengalaman properti dari kenyamanan ruang Anda saat ini. Tur virtual kami menawarkan panduan berjalan melalui rumah secara immersif, memungkinkan Anda membayangkan diri Anda tinggal di setiap ruang sebelum membuat jadwal kunjungan secara langsung. Menghemat waktu Anda dan memastikan Anda fokus pada properti yang benar-benar resonansi dengan Anda.",
+      "Feature - Expert Real Estate Agents":
+        "Terhubunglah dengan profesional real estat berpengalaman yang siap membantu Anda membuat keputusan yang terinformasi. Jaringan agen kami siap membimbing Anda melalui setiap langkah proses pembelian, memberikan wawasan, menjawab pertanyaan, dan bernegosiasi atas nama Anda.",
+      "Feature - Neighborhood Insights":
+        "Buat pilihan yang terinformasi tidak hanya tentang propertinya, tetapi juga lingkungan di sekitarnya. Profind menyediakan wawasan berharga tentang komunitas lokal, sekolah, fasilitas, dan lainnya, memastikan Anda tidak hanya membeli rumah, tetapi juga gaya hidup.",
+      "Feature - Save Favorites and Alerts":
+        "Mudah untuk melacak properti yang menarik minat Anda dengan menyimpannya ke dalam daftar favorit. Selain itu, atur notifikasi untuk tetap mendapatkan informasi tentang daftar properti baru dan perubahan harga, sehingga Anda tidak akan melewatkan kesempatan untuk menemukan rumah impian Anda.",
+      "Feature - Seamless Mobile Experience":
+        "Cari properti saat Anda bepergian dengan situs web responsif seluler kami. Baik Anda sedang dalam perjalanan atau bersantai di rumah, Profind memastikan bahwa pencarian properti Anda tetap lancar dan dapat diakses dari perangkat apa pun.",
+      "Trusted by": "Dipercaya oleh orang-orang terkaya di dunia",
+      "Contact description":
+        "Mulailah perjalanan properti Anda dengan Profind hari ini dan temukan dunia kemungkinan. Rumah impian Anda hanya sejauh satu klik!",
+    },
+  },
+};
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+export default function Hero({ translate }) {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const navigation = [
+    // { name: 'Product', href: '#' },
+    { name: translate("Navbar Features"), href: "#" },
+    // { name: '', href: '#' },
+    { name: translate("Navbar Company"), href: "#" },
+  ];
 
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Profind</span>
@@ -38,18 +42,30 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {translate("Login Button")} <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
@@ -88,7 +104,7 @@ export default function Example() {
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
+                    {translate("Login Button")}
                   </a>
                 </div>
               </div>
@@ -106,38 +122,37 @@ export default function Example() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              More location just opens!{' '}
+              {translate("Breadcrumbs Main")}{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
-                Find out <span aria-hidden="true">&rarr;</span>
+                {translate("Breadcrumbs Action")} <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Find cheaper properties than any source, near you.
+              {translate("Tagline")}
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              {translate("Hero Short Description")}
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                {translate("Hero CTA")}
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
@@ -149,11 +164,11 @@ export default function Example() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
